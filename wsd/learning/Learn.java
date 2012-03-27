6d14f9330d059d2ba71cc8e3691b607c253d841c
Saeed Abdullah <sma249@cornell.edu>
2012-03-26 21:37:20 -0400

    Enables dynamic loading of classifiers.
    Inspired from callClassifier.java in [1], this patch lets the user specify
    the particular classifiy to train on the given data.
    
    It takes the following command-line arguments:
        -J => Job description, should be either train or test.
        -S => Location of the serialized classifier.
        -F => Arff file to test or train.
        -E => File to write evaluation output.
        -C => Classifier name to use.
    
    [1]: http://weka.wikispaces.com/Primer

--- wsd/learning/Learn.java
+++ wsd/learning/Learn.java
@@ -2,6 +2,7 @@
 import weka.core.converters.ConverterUtils.DataSource;
 import weka.classifiers.Classifier;
 import weka.core.Instances;
+import weka.core.Utils;
 
 import weka.classifiers.trees.J48;
 
@@ -62,6 +63,32 @@ class Train {
     private Classifier classifier;
     private Instances trainInstances;
 
+    public Train(String [] args) throws Exception {
+        /* Constructs classifier by using options from the string array.
+         *
+         * param
+         * ----
+         *  args: Array of options. The following flags are in use:
+         *      -S => File path to output serialized classifier.
+         *      -F => Train arff file.
+         *      -C => Classifier name and options. If no classifier is
+         *          specified, J48() is used by default.
+         */
+
+
+        String trainFilePath = Utils.getOption('F', args);
+        String outputPath = Utils.getOption('S', args);
+        String classifierName = Utils.getOption('C', args);
+        if (classifierName.length() == 0)
+            new Train(trainFilePath, outputPath);
+        else {
+            Classifier classifier = Classifier.forName(classifierName,
+                    args);
+
+            new Train(trainFilePath, outputPath, classifier);
+        }
+    }
+
     public Train(String trainFilePath, String outputPath)
         throws Exception {
         /*
@@ -80,10 +107,42 @@ class Train {
         this.saveClassifier(outputPath);
     }
 
+     public Train(String trainFilePath, String outputPath,
+             Classifier classifier) throws Exception {
+        /*
+         * Performs the default tasks. It first created a classifier
+         * to train on the dataset and then the classifier is serialized
+         * to the given file.
+         * param
+         * ----
+         *  trainFilePath: Training file location.
+         *  outputPath: File path to which the classifier would be
+         *      serialized.
+         *  classifier: Classifier instance.
+         */
+
+        this.setDataInstances(trainFilePath);
+        this.buildClassifier(classifier);
+        this.saveClassifier(outputPath);
+    }
 
 
     public void setDataInstances(String filename) throws Exception {
         this.trainInstances = Util.getData(filename);
+    }
+
+    public void buildClassifier(Classifier classifier)
+        throws Exception {
+        /*
+         * Builds classifier.
+         *
+         * param
+         * ----
+         *  classifier: Classifier to be used for this class.
+         */
+
+        classifier.buildClassifier(this.trainInstances);
+        this.classifier = classifier;
     }
 
 
@@ -97,8 +156,6 @@ class Train {
          *  classifier: Classifier to be used for this class.
          *  options: String options to be used.
          *
-         * Needs to re-implement this so that instead of supplying 
-         * actual instance of classifier, a string name would suffice.
          */
 
         classifier.setOptions(weka.core.Utils.splitOptions(options));
@@ -139,6 +196,25 @@ class Evaluate {
 
     private Classifier classifier;
     private Instances testInstances;
+
+    public Evaluate(String args[]) throws Exception {
+        /* Peforms evaluation by using options from the string array.
+         *
+         * param
+         * ----
+         *  args: Array of options. The following flags are in use:
+         *      -S => Serialized classifier file.
+         *      -F => Test arff file.
+         *      -E => Output path to write evaluation result in kaggle
+         *          format.
+         */
+
+        String serializedOutput = Utils.getOption('S', args);
+        String testFilePath = Utils.getOption('F', args);
+        String outputPath = Utils.getOption('E', args);
+
+        new Evaluate(serializedOutput, testFilePath, outputPath);
+    }
 
     public Evaluate(String serializedOutput, String testFilePath,
             String outputPath) throws Exception {
@@ -208,22 +284,22 @@ class Evaluate {
 
 public class Learn {
     public static void main(String args[]) throws Exception {
-        String train = "train";
-        String test = "test";
-        if (args[0].equals(train)) {
-            /* Performs training.
-             * First param is the input source and the second param
-             * is the serialization output path.
-             */
-            new Train(args[1], args[2]);
-        } else if (args[0].equals(test)) {
-            /* Evaluates the test data.
-             * First param is the serialized classifier, second param
-             * denotes data location and the third param is the output
-             * path.
-             */
-            new Evaluate(args[1], args[2], args[3]);
-        }
+        String helpString = "Usage: Learn -J [train|test] "
+            + "-S serialized-file-location -F arff-file "
+            + "[-E evaluation-output] [-C classifierName] "
+            + "[classifier-options]\n";
+        String job = Utils.getOption('J', args);
+
+        if (job.length() == 0) {
+            throw new Exception(helpString + "\n" +
+                    "No Job description given.");
+        } else if (job.equals("train"))
+            new Train(args);
+        else if (job.equals("test"))
+            new Evaluate(args);
+        else
+            throw new Exception(helpString + "\n" +
+                    "Unknown Job description given.");
     }
 }
 
--- wsd/learning/run_script.sh
+++ wsd/learning/run_script.sh
@@ -2,7 +2,13 @@
 javac -cp .:lib/weka.jar Learn.java
 
 # Train
-java -cp .:lib/weka.jar Learn train /tmp/input.arff /tmp/classifier.serialized
+java -cp .:lib/weka.jar Learn -J train -F /tmp/input.arff -S /tmp/classifier.serialized
 
 # Test
-java -cp .:lib/weka.jar Learn test /tmp/classifier.serialized /tmp/test.arff /tmp/output.txt
+java -cp .:lib/weka.jar Learn -J test -S /tmp/classifier.serialized -F /tmp/test.arff -E /tmp/output.txt
+
+# Zero-R Train
+java -cp .:lib/weka.jar Learn -J train -F /tmp/input.arff -S /tmp/zeror.serialized -C weka.classifiers.rules.ZeroR
+
+# Test
+java -cp .:lib/weka.jar Learn -J test -S /tmp/zeror.serialized -F /tmp/test.arff -E /tmp/zeror.output.txt
